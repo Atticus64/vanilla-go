b976d2df162109aacc3920b4cb1c3e7bb069e936
Jonathan <jonathanelian64@gmail.com>
2023-04-19 17:07:39 -0700

    feat: Add Put and Delete methods to api notes

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/atticus64/api-golang
 
 go 1.19
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 )
 
 type Note struct {
+	Id          string `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 }
@@ -16,38 +22,76 @@ type Note struct {
 var notes []Note
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/notes", notesHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/notes", getNotes)
+	r.Get("/notes/{id}", noteById)
+	r.Put("/notes/{id}", UpdateNote)
+	r.Post("/notes", addNote)
+	r.Delete("/notes/{id}", deleteNote)
 
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome to the API!")
 }
 
-func notesHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		getNotes(w, r)
-	case http.MethodPost:
-		addNote(w, r)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+func getNotesDB() []Note {
+	notesJson, err := os.OpenFile("notes.json", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
 	}
+
+	defer notesJson.Close()
+
+	data, err := notesJson.Stat()
+
+	if err != nil {
+		panic(err)
+	}
+
+	var notes []Note
+	if data.Size() != 0 {
+		bytes, err := io.ReadAll(notesJson)
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal(bytes, &notes)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return notes
 }
 
 func getNotes(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	notesJson, err := ioutil.ReadFile("notes.json")
+	notesJson, err := os.OpenFile("notes.json", os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		ioutil.WriteFile("notes.json", []byte("[]"), 0644)
+		panic(err)
 	}
 
-	notes := []Note{}
-	if notesJson != nil {
-		json.Unmarshal(notesJson, &notes)
+	defer notesJson.Close()
+
+	data, err := notesJson.Stat()
+
+	if err != nil {
+		panic(err)
+	}
+
+	var notes []Note
+	if data.Size() != 0 {
+		bytes, err := io.ReadAll(notesJson)
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal(bytes, &notes)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	json.NewEncoder(w).Encode(notes)
@@ -61,10 +105,125 @@ func addNote(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	note.Id = uuid.NewString()
+
 	notes = append(notes, note)
-	fmt.Println(notes)
-	file, _ := json.MarshalIndent(notes, "", " ")
-	_ = ioutil.WriteFile("notes.json", file, 0644)
+	file, err := json.MarshalIndent(notes, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile("notes.json", file, 0644)
+	if err != nil {
+		panic(err)
+	}
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Note added"))
+	json.NewEncoder(w).Encode(note)
+}
+
+func searchNote(id string, notes []Note) Note {
+	for _, n := range notes {
+		if n.Id == id {
+			return n
+		}
+	}
+	return Note{
+		Id: "not found",
+	}
+}
+
+func removeNoteById(notes []Note, id string) []Note {
+	// Creamos un slice de personas vacío
+	result := []Note{}
+
+	// Iteramos sobre el slice original y agregamos todas las personas
+	// excepto la que tenga el nombre indicado
+	for _, note := range notes {
+		if note.Id != id {
+			result = append(result, note)
+		}
+	}
+
+	return result
+}
+
+func deleteNote(w http.ResponseWriter, r *http.Request) {
+	noteId := chi.URLParam(r, "id")
+
+	notes := getNotesDB()
+
+	noteFinded := searchNote(noteId, notes)
+
+	if noteFinded.Id == "not found" {
+		w.Write([]byte(fmt.Sprintf("Not found Note with id: %d", &noteId)))
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	newNotes := removeNoteById(notes, noteId)
+
+	file, err := json.MarshalIndent(newNotes, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile("notes.json", file, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(newNotes)
+}
+
+func noteById(w http.ResponseWriter, r *http.Request) {
+	noteId := chi.URLParam(r, "id")
+
+	notes := getNotesDB()
+
+	noteFinded := searchNote(noteId, notes)
+
+	if noteFinded.Id == "not found" {
+		w.Write([]byte(fmt.Sprintf("Not found Note with id: %d", &noteId)))
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(notes)
+}
+
+func UpdateNote(w http.ResponseWriter, r *http.Request) {
+	noteId := chi.URLParam(r, "id")
+
+	notes := getNotesDB()
+
+	noteFinded := searchNote(noteId, notes)
+
+	if noteFinded.Id == "not found" {
+		w.Write([]byte(fmt.Sprintf("Not found Note with id: %d", &noteId)))
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	var note Note
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	note.Id = noteFinded.Id
+
+	notes = removeNoteById(notes, noteId)
+
+	notes = append(notes, note)
+
+	file, err := json.MarshalIndent(notes, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile("notes.json", file, 0644)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(notes)
 }
